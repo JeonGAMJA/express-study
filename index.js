a150609722f6f9473d7e6797dfe68eb9aad46bc9
GamjaJEON <indian_potato@naver.com>
2024-04-01 10:40:46 +0900

    feat: 토큰을 이용해서 요청 보내기

--- index.js
+++ index.js
@@ -25,9 +25,22 @@ app.post('/login', (req, res) => {
   res.json({ accessToken: accessToken });
 });
 
-app.get('/posts', (req, res) => {
+app.get('/posts', authMiddleware, (req, res) => {
   res.json(posts);
 });
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers['authrizations'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretText, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 const port = 4000;
 app.listen(port, () => {
