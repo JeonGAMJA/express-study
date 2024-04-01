cd73e59d8c2df637335d3c38b7924551fd1588e6
GamjaJEON <indian_potato@naver.com>
2024-04-01 12:17:41 +0900

    feat: 로그인 회원가입을 위한 화면 구현하기

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// view engine setup
+app.set('view', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 mongoose.set('strictQuery', false);
 
 mongoose
@@ -18,9 +24,21 @@ mongoose
     console.log(err);
   });
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+app.post('/login', (req, res, next) => {});
+
+app.get('/signup', function (req, res, next) {
+  res.render('signup');
+});
+
+app.post('/signup', (req, res, next) => {});
+
 const port = 4000;
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
-
-app.use('/static', express.static(path.join(__dirname, 'public')));
