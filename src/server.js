d5caaf0743c73f5de86462d5f09c334e85841627
GamjaJEON <indian_potato@naver.com>
2024-04-03 23:11:18 +0900

    feat: 로그아웃 기능 만들기

--- src/server.js
+++ src/server.js
@@ -79,6 +79,15 @@ app.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
+app.post('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/login');
+  });
+});
+
 app.get('/signup', checkNotAuthenticated, function (req, res, next) {
   res.render('signup');
 });
